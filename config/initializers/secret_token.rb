82fd5ae3e420613b604c9f6e7e6a467b32a36c6d
chrisaped <chrisaped@gmail.com>
2013-07-02 14:47:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filmtrigger::Application.config.secret_token = '071ca17f67e8c85ead48ae1cb59512c0723b07686158b7ebc88a0c5721d15084aa5bac72095df09fa18c252b1fba0beb6ce60742e259ba1fdfbeaca83b4770a8'
